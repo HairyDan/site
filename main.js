1206e07cadb0e0bbd85521572fd1e0b9cb5d846d
Daniel Schormans <danielschormans@googlemail.com>
2018-03-21 00:41:41 +0000

    fixed positioning on resize (changed divs), played with colours

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var c = document.getElementById("myCanvas");
-c.width = window.innerWidth;
-c.height = window.innerHeight;
-var ctx = c.getContext("2d");
+//var c = document.getElementById("myCanvas");
+//c.width = window.innerWidth;
+//c.height = window.innerHeight;
+//var ctx = c.getContext("2d");
 //ctx.fillStyle="#CCCCCC";
 //ctx.fillRect(0,0,c.width,c.height);
 /*
@@ -11,22 +11,23 @@ for (i = 1; i < 5; i++) {
   ctx.fillStyle = "gray";
   ctx.fill();
 }*/
-
-var paper = Raphael(0, 0, c.width, c.height);
-var circle1 = paper.circle(c.width / 5, c.height / 2, 30);
+var bod = document.getElementById("raphdiv");
+var paper = Raphael(bod, "100%", "80%");
+var circle1 = paper.circle("20%", "50%", 30);
 var defaultcirc = [
     {
         fill: "#aaa",
         stroke: "#eee",
         "stroke-width": 2
+
     }
 ];
 circle1.attr(defaultcirc);
-var circle2 = paper.circle(c.width / 5 * 2, c.height / 2, 30);
+var circle2 = paper.circle("40%", "50%", 30);
 circle2.attr(defaultcirc);
-var circle3 = paper.circle(c.width / 5 * 3, c.height / 2, 30);
+var circle3 = paper.circle("60%", "50%", 30);
 circle3.attr(defaultcirc);
-var circle4 = paper.circle(c.width / 5 * 4, c.height / 2, 30);
+var circle4 = paper.circle("80%", "50%", 30);
 circle4.attr(defaultcirc);
 var circles = [];
 circles.push(circle1);
